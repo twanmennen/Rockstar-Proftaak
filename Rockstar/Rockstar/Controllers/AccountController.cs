51349e0a9497442b7fe2e7162cf5c9c7b9502e6c
twanmennen112 <twan.mennen@student.fontys.nl>
2018-09-10 14:17:35 +0200

    initial commit

---
+++ Rockstar/Rockstar/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Rockstar.ViewModels;
+
+namespace Rockstar.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View(new LogInViewModel("hoi"));
+        }
+    }
+}
---
+++ Rockstar/Rockstar/ViewModels/LogInViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rockstar.ViewModels
+{
+    public class LogInViewModel
+    {
+        public string aap;
+
+        public LogInViewModel(string _aap)
+        {
+            aap = _aap;
+        }
+    }
+}
